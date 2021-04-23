ca1fdb9cf3335b0eb515c80be69ee9666dd2d85c
Alex Ellner <alex.ellner@siemens.com>
2021-04-23 08:04:04 +0200

    npm install wip

---
+++ backend-new/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd54d4c67297a368a53a2881fc81bd2c'),
+    },
+  },
+});
---
+++ backend-new/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e260577e-9bf4-43c5-b72f-294fcee9301f'
+};
